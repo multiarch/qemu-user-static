806f2f8c396967bd39671e15002663ea9e3d71d3
Manfred Touron <m@42.am>
2015-12-21 14:48:38 +0100

    Bump qemu-user-static@2.5.0 + handle from_arch + new archs woot

--- update.sh
+++ update.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
-archs="aarch64 alpha arm armeb cris i386 m68k microblaze microblazeel mips mips64 mips64el mipsel mipsn32 mipsn32el or32 ppc ppc64 ppc64abi32 ppc64le s390x sh4 sh4eb sparc sparc32plus sparc64 unicore32 x86_64"
+from_archs="x86_64"
+to_archs="$(cat all-archs)"
 
-for arch in $archs; do
-    mkdir -p archs/$arch
-    cat > archs/$arch/Dockerfile <<EOF
+for from_arch in $from_archs; do
+    for to_arch in $to_archs; do
+	mkdir -p archs/$from_arch-$to_arch
+	cat > archs/$from_arch-$to_arch/Dockerfile <<EOF
 FROM scratch
-ADD https://github.com/multiarch/qemu-user-static/releases/download/v2.0.0/amd64_qemu-$arch-static.tar.gz /usr/bin
+ADD https://github.com/multiarch/qemu-user-static/releases/download/v2.5.0/${from_arch}_qemu-${to_arch}-static.tar.xz /usr/bin
 EOF
-    docker build -t multiarch/qemu-user-static:$arch archs/$arch
+	docker build -t multiarch/qemu-user-static:$from_arch-$to_arch archs/$from_arch-$to_arch
+	if [ "$from_arch" = "x86_64" ]; then
+	    docker tag -f multiarch/qemu-user-static:$from_arch-$to_arch multiarch/qemu-user-static:$to_arch 
+	fi
+    done
 done
 
-
 docker build -t multiarch/qemu-user-static:register register
-
