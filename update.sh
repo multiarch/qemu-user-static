720a9e53ba5afb3fbb4e5ef068b85bd68697e6e3
Manfred Touron <m@42.am>
2017-01-18 20:51:30 +0100

    Bump qemu v2.8.0 (fix #16)

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ for from_arch in $from_archs; do
 	mkdir -p archs/$from_arch-$to_arch
 	cat > archs/$from_arch-$to_arch/Dockerfile <<EOF
 FROM scratch
-ADD https://github.com/multiarch/qemu-user-static/releases/download/v2.7.0/${from_arch}_qemu-${to_arch}-static.tar.gz /usr/bin
+ADD https://github.com/multiarch/qemu-user-static/releases/download/v2.8.0/${from_arch}_qemu-${to_arch}-static.tar.gz /usr/bin
 EOF
 	docker build -t multiarch/qemu-user-static:$from_arch-$to_arch archs/$from_arch-$to_arch
 	if [ "$from_arch" = "x86_64" ]; then
