ae209866d9c20d44b78b7784d360af1bac803f60
Manfred Touron <m@42.am>
2017-01-18 10:35:13 +0100

    build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,5 @@
 
 set -xe
 
-wget -N http://ftp.fr.debian.org/debian/pool/main/q/qemu/qemu-user-static_2.7+dfsg-3+b1_amd64.deb
-dpkg -i qemu-user-static_2.7+dfsg-3+b1_amd64.deb
-
+wget -N http://ftp.fr.debian.org/debian/pool/main/q/qemu/qemu-user-static_2.8+dfsg-1_amd64.deb
+dpkg -i qemu-user-static_2.8+dfsg-1_amd64.deb
