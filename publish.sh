6a2bc8cdaed1c6948344b72427e7135ff312b989
1138-4EB <1138-4EB@users.noreply.github.com>
2019-02-19 19:28:42 +0100

    fix: allow to test feature branches without deploying the artifacts

--- publish.sh
+++ publish.sh
@@ -28,34 +28,38 @@ for file in *; do
     cp $file.tar.gz x86_64_$file.tar.gz
 done
 
-# create a release
-release_id=$(curl -sL -X POST \
-    -H "Content-Type: application/json" \
-    -H "Accept: application/vnd.github.v3+json" \
-    -H "Authorization: token ${GITHUB_TOKEN}" \
-    -H "Cache-Control: no-cache" -d "{
-  \"tag_name\": \"v${VERSION}\",
-  \"target_commitish\": \"master\",
-  \"name\": \"v${VERSION}\",
-  \"body\": \"# \`qemu-*-static\` @ ${VERSION}\",
-  \"draft\": false,
-  \"prerelease\": false
-}" "https://api.github.com/repos/${REPO}/releases" | jq -r ".id")
-if [ "$release_id" = "null" ]; then
-    # get the existing release id
-    release_id=$(set -x; curl -sL \
-    -H "Content-Type: application/json" \
-    -H "Accept: application/vnd.github.v3+json" \
-    -H "Authorization: token ${GITHUB_TOKEN}" \
-    -H "Cache-Control: no-cache" \
-    "https://api.github.com/repos/${REPO}/releases" | jq -r --arg version "${VERSION}" '.[] | select(.name == "v"+$version).id')
-fi
+if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
 
-for file in *; do
-    content_type=$(file --mime-type -b ${file})
-    curl -sL \
-        -H "Authorization: token ${GITHUB_TOKEN}" \
-        -H "Content-Type: ${content_type}" \
-        --upload-file ${file} \
-        "https://uploads.github.com/repos/${REPO}/releases/${release_id}/assets?name=${file}"
-done
+  # create a release
+  release_id=$(curl -sL -X POST \
+      -H "Content-Type: application/json" \
+      -H "Accept: application/vnd.github.v3+json" \
+      -H "Authorization: token ${GITHUB_TOKEN}" \
+      -H "Cache-Control: no-cache" -d "{
+    \"tag_name\": \"v${VERSION}\",
+    \"target_commitish\": \"master\",
+    \"name\": \"v${VERSION}\",
+    \"body\": \"# \`qemu-*-static\` @ ${VERSION}\",
+    \"draft\": false,
+    \"prerelease\": false
+  }" "https://api.github.com/repos/${REPO}/releases" | jq -r ".id")
+  if [ "$release_id" = "null" ]; then
+      # get the existing release id
+      release_id=$(set -x; curl -sL \
+      -H "Content-Type: application/json" \
+      -H "Accept: application/vnd.github.v3+json" \
+      -H "Authorization: token ${GITHUB_TOKEN}" \
+      -H "Cache-Control: no-cache" \
+      "https://api.github.com/repos/${REPO}/releases" | jq -r --arg version "${VERSION}" '.[] | select(.name == "v"+$version).id')
+  fi
+
+  for file in *; do
+      content_type=$(file --mime-type -b ${file})
+      curl -sL \
+          -H "Authorization: token ${GITHUB_TOKEN}" \
+          -H "Content-Type: ${content_type}" \
+          --upload-file ${file} \
+          "https://uploads.github.com/repos/${REPO}/releases/${release_id}/assets?name=${file}"
+  done
+
+fi
